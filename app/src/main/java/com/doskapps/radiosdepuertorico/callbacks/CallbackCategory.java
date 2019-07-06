3a651bd297c1f2422baf3e9f56b4065b61d9bcc4
doskapps <50601180+doskapps@users.noreply.github.com>
2019-07-06 02:17:49 -0300

    Initial commit

---
+++ app/src/main/java/com/doskapps/radiosdepuertorico/callbacks/CallbackCategory.java
@@ -0,0 +1,13 @@
+package com.doskapps.radiosdepuertorico.callbacks;
+
+import com.doskapps.radiosdepuertorico.models.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CallbackCategory {
+
+    public String status = "";
+    public int count = -1;
+    public List<Category> categories = new ArrayList<>();
+}
---
+++ app/src/main/java/com/doskapps/radiosdepuertorico/callbacks/CallbackPais.java
@@ -0,0 +1,12 @@
+package com.doskapps.radiosdepuertorico.callbacks;
+
+import com.doskapps.radiosdepuertorico.models.Pais;
+
+import java.util.ArrayList;
+
+public class CallbackPais {
+
+    public String status = "";
+    public int count_total = -1;
+    public ArrayList<Pais> posts = new ArrayList<>();
+}
