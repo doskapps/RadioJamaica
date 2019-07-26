19fbe637537f3f1ce9d251b04a88e70cb64ff884
doskapps <50601180+doskapps@users.noreply.github.com>
2019-07-25 23:20:25 -0300

    Nueva App Radio Guatemala

---
+++ app/src/main/java/com/doskapps/radiosdeguatemala/callbacks/CallbackCategory.java
@@ -0,0 +1,13 @@
+package com.doskapps.radiosdeguatemala.callbacks;
+
+import com.doskapps.radiosdeguatemala.models.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CallbackCategory {
+
+    public String status = "";
+    public int count = -1;
+    public List<Category> categories = new ArrayList<>();
+}
