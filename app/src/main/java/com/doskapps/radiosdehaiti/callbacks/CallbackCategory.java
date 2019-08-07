6a5680a5aa9c0c9a74940a85974a16c390edb33a
doskapps <50601180+doskapps@users.noreply.github.com>
2019-08-07 14:43:54 -0300

    Nuew App Haiti Radio

---
+++ app/src/main/java/com/doskapps/radiosdehaiti/callbacks/CallbackCategory.java
@@ -0,0 +1,13 @@
+package com.doskapps.radiosdehaiti.callbacks;
+
+import com.doskapps.radiosdehaiti.models.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CallbackCategory {
+
+    public String status = "";
+    public int count = -1;
+    public List<Category> categories = new ArrayList<>();
+}
